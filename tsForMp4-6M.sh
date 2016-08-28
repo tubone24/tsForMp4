235556aff926ba5b96b332a186ba1a1be8d18a12
tubone <meitantei-conan@live.jp>
2016-08-28 15:46:39 +0900

    地デジ最適化6M

--- tsForMp4-6M.sh
+++ tsForMp4-6M.sh
@@ -9,7 +9,7 @@ do
     targetFileName="$(find  $(echo $tsFolder)*.m2ts | head -n 1)"
     newMp4FileName=$(echo $mp4Folder)$(basename $(echo $targetFileName | sed 's/.m2ts/.mp4/g'))
     echo "$(echo $targetFileName) encording... to $(echo $newMp4FileName)"
-    /home/chinachu/chinachu/usr/bin/ffmpeg -v 0 -re -i $(echo $targetFileName) -ss 2 -threads auto -c:v libx264 -c:a aac -s 1920x1080 -filter:v yadif -b:v 6M -b:a 192k -profile:v baseline -preset ultrafast -tune fastdecode,zerolatency -movflags frag_keyframe+empty_moov+faststart+default_base_moof -y -f mp4 $(echo $newMp4FileName)
+    /home/chinachu/chinachu/usr/bin/ffmpeg -v 0 -re -i $(echo $targetFileName) -ss 2 -threads auto -c:v libx264 -c:a aac -s 1440x1080 -filter:v yadif -b:v 6M -b:a 192k -profile:v baseline -preset ultrafast -tune fastdecode,zerolatency -movflags frag_keyframe+empty_moov+faststart+default_base_moof -y -f mp4 $(echo $newMp4FileName)
     if [ $? = 0 ]; then
       echo "正常にエンコード終了"
       counter=$(( counter + 1 ))
